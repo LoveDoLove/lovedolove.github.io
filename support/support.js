2f3db10c125506f0273a83eb08db13f9d0f3c6a0
LoveDoLove <v0130p5100cuboss@gmail.com>
2022-09-01 17:37:46 +0800

    update

--- support/support.js
+++ support/support.js
@@ -1,13 +1,22 @@
 window.onload = init;
+window.onload = errorfunction;
+
 
 function init() {
    document.forms[0].onsubmit = function() {
       if (this.checkValidity()) alert("Data passes");
       return false;
    }
-   
-   document.getElementById("xxx").onclick = turnOnA;
    document.getElementById("xxx").onclick=turnOnB;
+}
+
+function errorfunction()
+{
+   document.getElementById("error").onclick = function() 
+   {
+      alert("Still In Developing...");
+      return true;
+   }
 }
 
 function turnOnA() {

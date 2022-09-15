695404388ecb000c5ce46ade1d2bfd906064408a
LoveDoLove <v0130p5100cuboss@gmail.com>
2022-09-15 13:49:28 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,55 @@
 window.onload = startup;
 
+var Base64= {
+	_keyStr: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(e){var t="";
+	var n,r,i,s,o,u,a;var f=0;e=Base64._utf8_encode(e);while(f<e.length){n=e.charCodeAt(f++);r=e.charCodeAt(f++);i=e.charCodeAt(f++);s=n>>2;o=(n&3)<<4|r>>4;u=(r&15)<<2|i>>6;a=i&63;if(isNaN(r)){u=a=64
+}
+
+else if(isNaN(i)) {
+	a=64
+}
+
+t=t+this._keyStr.charAt(s)+this._keyStr.charAt(o)+this._keyStr.charAt(u)+this._keyStr.charAt(a)
+}
+
+return t
+},decode:function(e) {
+var t="";var n,r,i;var s,o,u,a;var f=0;e=e.replace(/[^A-Za-z0-9\+\/\=]/g,"");while(f<e.length){s=this._keyStr.indexOf(e.charAt(f++));o=this._keyStr.indexOf(e.charAt(f++));u=this._keyStr.indexOf(e.charAt(f++));a=this._keyStr.indexOf(e.charAt(f++));n=s<<2|o>>4;r=(o&15)<<4|u>>2;i=(u&3)<<6|a;t=t+String.fromCharCode(n);if(u!=64){t=t+String.fromCharCode(r)
+}
+
+if(a!=64) {
+t=t+String.fromCharCode(i)
+}}
+
+t=Base64._utf8_decode(t);return t
+},_utf8_encode:function(e) {
+e=e.replace(/\r\n/g,"\n");var t="";for(var n=0;n<e.length;n++){var r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r)
+}
+
+else if(r>127&&r<2048) {
+t+=String.fromCharCode(r>>6|192);t+=String.fromCharCode(r&63|128)
+}
+
+else {
+t+=String.fromCharCode(r>>12|224);t+=String.fromCharCode(r>>6&63|128);t+=String.fromCharCode(r&63|128)
+}}
+
+return t
+},_utf8_decode:function(e) {
+var t="";var n=0;var r=c1=c2=0;while(n<e.length){r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r);n++
+}
+
+else if(r>191&&r<224) {
+c2=e.charCodeAt(n+1);t+=String.fromCharCode((r&31)<<6|c2&63);n+=2
+}
+
+else {
+c2=e.charCodeAt(n+1);c3=e.charCodeAt(n+2);t+=String.fromCharCode((r&15)<<12|(c2&63)<<6|c3&63);n+=3
+}}
+
+return t
+}}
+
 function startup()
 {
    var tag = document.getElementById("tag");
@@ -14,6 +64,18 @@ function startup()
    var adb = document.getElementById("adb");
    var support = document.getElementById("support");
 
+   var t1 = Base64.decode('QExvdmVEb0xvdmU=');
+   var t2 = Base64.decode('WW91dHViZQ==');
+   var t3 = Base64.decode('R2l0aHVi');
+   var t4 = Base64.decode('VHdpdHRlcg==');
+   var t5 = Base64.decode('RGlzY29yZA==');
+   var t6 = Base64.decode('VGVsZWdyYW0=');
+   var t7 = Base64.decode('RG9uYXRl');
+   var t8 = Base64.decode('Q2xpY2sgVG8gU3RhcnQgVXNpbmcgVGhlIFdlYnNpdGU=');
+   var t9 = Base64.decode('TG92ZURvTG92ZSBTdG9yZQ==');
+   var t10 = Base64.decode('QURCIFR1dG9yaWFs');
+   var t11 = Base64.decode('U3VwcG9ydA==');
+
    yt.addEventListener("click", a);
    git.addEventListener("click", b);
    twitter.addEventListener("click", c);
@@ -25,68 +87,78 @@ function startup()
    adb.addEventListener("click", i);
    support.addEventListener("click", j);
 
-   tag.innerText = "@LoveDoLove";
-   yt.innerText = "Youtube";
-   git.innerText = "Github";
-   twitter.innerText = "Twitter";
-   dc.innerText = "Discord";
-   telegram.innerText = "Telegram";
-   donate.innerText = "Donate";
-   main.innerText = "Click To Start Using The Website";
-   store.innerText = "LoveDoLove Store";
-   adb.innerText = "ADB Tutorial";
-   support.innerText = "Support";
+   tag.innerText = t1;
+   yt.innerText = t2;
+   git.innerText = t3;
+   twitter.innerText = t4;
+   dc.innerText = t5;
+   telegram.innerText = t6;
+   donate.innerText = t7;
+   main.innerText = t8;
+   store.innerText = t9;
+   adb.innerText = t10;
+   support.innerText = t11;
 }
 
 function a()
 {
-   window.open("https://www.youtube.com/channel/UCh6Zv3sfraWhLir9nCXT7nw");
+   var a = Base64.decode('aHR0cHM6Ly93d3cueW91dHViZS5jb20vY2hhbm5lbC9VQ2g2WnYzc2ZyYVdoTGlyOW5DWFQ3bnc=');
+   window.open(a);
 }
 
 function b()
 {
-   window.open("https://github.com/LoveDoLove");
+   var a = Base64.decode('aHR0cHM6Ly9naXRodWIuY29tL0xvdmVEb0xvdmU=');
+   window.open(a);
 }
 
 function c()
 {
-   window.open("https://twitter.com/LoveDoLove1");
+   var a = Base64.decode('aHR0cHM6Ly90d2l0dGVyLmNvbS9Mb3ZlRG9Mb3ZlMQ==');
+   window.open(a);
 }
 
 function d()
 {
-   window.open("https://discord.gg/4b4uDMqVCy");
+   var a = Base64.decode('aHR0cHM6Ly9kaXNjb3JkLmdnLzRiNHVETXFWQ3k=');
+   window.open(a);
 }
 
 function e()
 {
-   window.open("https://t.me/LoveDoLoveChatsGroup");
+   var a = Base64.decode('aHR0cHM6Ly90Lm1lL0xvdmVEb0xvdmVDaGF0c0dyb3Vw');
+   window.open(a);
 }
 
 function f()
 {
-   window.open("https://bit.ly/31eTEXh");
+   var a = Base64.decode('aHR0cHM6Ly9iaXQubHkvMzFlVEVYaA==');
+   window.open(a);
 }
 
 function g()
 {
-   var openMain = document.src = "/main/main.html";
+   var a = Base64.decode('L21haW4vbWFpbi5odG1s');
+   var openMain = document.src = a;
    window.open(openMain);
 }
 
 function h()
 {
-   var openStore = document.src = "/store/store.html";
+   var a = Base64.decode('L3N0b3JlL3N0b3JlLmh0bWw=');
+   var openStore = document.src = a;
    window.open(openStore);
 }
 
 function i()
 {
-   var openAdb = document.src = "/adb/adb.html";
+   var a = Base64.decode('L2FkYi9hZGIuaHRtbA==');
+   var openAdb = document.src = a;
    window.open(openAdb);
 }
 
 function j()
 {
-   window.alert("Sill in developing...");
+   var a = Base64.decode('U2lsbCBpbiBkZXZlbG9waW5nLi4u');
+   window.alert(a);
 }

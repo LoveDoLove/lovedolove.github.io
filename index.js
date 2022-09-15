99fb22d2b4d28ac0893c90f2b183fec789b4fc35
LoveDoLove <v0130p5100cuboss@gmail.com>
2022-09-15 09:11:39 +0800

    update

---
+++ index.js
@@ -0,0 +1,11 @@
+//for notice
+window.onload = asupport;
+
+function asupport()
+{
+   document.getElementById("error").onclick = function() 
+   {
+      alert("Still In Developing...");
+      return true;
+   }
+}

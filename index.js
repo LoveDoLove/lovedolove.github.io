254137961a3588ffd5e347206f7ba1be2f10e3e7
LoveDoLove <v0130p5100cuboss@gmail.com>
2022-09-16 13:42:29 +0800

    update

--- index.js
+++ index.js
@@ -1,139 +1,134 @@
 window.onload = startup;
-
-var Base64= {
-	_keyStr: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(e){var t="";
-	var n,r,i,s,o,u,a;var f=0;e=Base64._utf8_encode(e);while(f<e.length){n=e.charCodeAt(f++);r=e.charCodeAt(f++);i=e.charCodeAt(f++);s=n>>2;o=(n&3)<<4|r>>4;u=(r&15)<<2|i>>6;a=i&63;if(isNaN(r)){u=a=64
+var Base64 = {
+    _keyStr: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
+    encode: function (t) {
+        var n,
+            r,
+            o,
+            s,
+            l,
+            m,
+            y,
+            B = "",
+            C = 0;
+        for (t = Base64._utf8_encode(t); C < t.length; )
+            (n = t.charCodeAt(C++)),
+                (r = t.charCodeAt(C++)),
+                (o = t.charCodeAt(C++)),
+                (s = n >> 2),
+                (l = ((3 & n) << 4) | (r >> 4)),
+                (m = ((15 & r) << 2) | (o >> 6)),
+                (y = 63 & o),
+                isNaN(r) ? (m = y = 64) : isNaN(o) && (y = 64),
+                (B = B + this._keyStr.charAt(s) + this._keyStr.charAt(l) + this._keyStr.charAt(m) + this._keyStr.charAt(y));
+        return B;
+    },
+    decode: function (t) {
+        var n,
+            r,
+            o,
+            s,
+            l,
+            m,
+            y,
+            B = "",
+            C = 0;
+        for (t = t.replace(/[^A-Za-z0-9\+\/\=]/g, ""); C < t.length; )
+            (s = this._keyStr.indexOf(t.charAt(C++))),
+                (l = this._keyStr.indexOf(t.charAt(C++))),
+                (m = this._keyStr.indexOf(t.charAt(C++))),
+                (y = this._keyStr.indexOf(t.charAt(C++))),
+                (n = (s << 2) | (l >> 4)),
+                (r = ((15 & l) << 4) | (m >> 2)),
+                (o = ((3 & m) << 6) | y),
+                (B += String.fromCharCode(n)),
+                64 != m && (B += String.fromCharCode(r)),
+                64 != y && (B += String.fromCharCode(o));
+        return Base64._utf8_decode(B);
+    },
+    _utf8_encode: function (t) {
+        t = t.replace(/\r\n/g, "\n");
+        for (var n = "", r = 0; r < t.length; r++) {
+            var o = t.charCodeAt(r);
+            o < 128
+                ? (n += String.fromCharCode(o))
+                : o > 127 && o < 2048
+                ? ((n += String.fromCharCode((o >> 6) | 192)), (n += String.fromCharCode((63 & o) | 128)))
+                : ((n += String.fromCharCode((o >> 12) | 224)), (n += String.fromCharCode(((o >> 6) & 63) | 128)), (n += String.fromCharCode((63 & o) | 128)));
+        }
+        return n;
+    },
+    _utf8_decode: function (t) {
+        for (var n = "", r = 0, o = (c1 = c2 = 0); r < t.length; )
+            (o = t.charCodeAt(r)) < 128
+                ? ((n += String.fromCharCode(o)), r++)
+                : o > 191 && o < 224
+                ? ((n += String.fromCharCode(((31 & o) << 6) | (63 & (c2 = t.charCodeAt(r + 1))))), (r += 2))
+                : ((n += String.fromCharCode(((15 & o) << 12) | ((63 & (c2 = t.charCodeAt(r + 1))) << 6) | (63 & (c3 = t.charCodeAt(r + 2))))), (r += 3));
+        return n;
+    },
+};
+function startup() {
+    var t = document.getElementById("tag"),
+        n = document.getElementById("youtube"),
+        r = document.getElementById("github"),
+        o = document.getElementById("twitter"),
+        s = document.getElementById("discord"),
+        l = document.getElementById("telegram"),
+        m = document.getElementById("donate"),
+        y = document.getElementById("main"),
+        B = document.getElementById("store"),
+        C = document.getElementById("adb"),
+        u = document.getElementById("support");
+    n.addEventListener("click", a),
+        r.addEventListener("click", b),
+        o.addEventListener("click", c),
+        s.addEventListener("click", d),
+        l.addEventListener("click", e),
+        m.addEventListener("click", f),
+        y.addEventListener("click", g),
+        B.addEventListener("click", h),
+        C.addEventListener("click", i),
+        u.addEventListener("click", j),
+        (t.innerText = Base64.decode("QExvdmVEb0xvdmU=")),
+        (n.innerText = Base64.decode("WW91dHViZQ==")),
+        (r.innerText = Base64.decode("R2l0aHVi")),
+        (o.innerText = Base64.decode("VHdpdHRlcg==")),
+        (s.innerText = Base64.decode("RGlzY29yZA==")),
+        (l.innerText = Base64.decode("VGVsZWdyYW0=")),
+        (m.innerText = Base64.decode("RG9uYXRl")),
+        (y.innerText = Base64.decode("Q2xpY2sgVG8gU3RhcnQgVXNpbmcgVGhlIFdlYnNpdGU=")),
+        (B.innerText = Base64.decode("TG92ZURvTG92ZSBTdG9yZQ==")),
+        (C.innerText = Base64.decode("QURCIFR1dG9yaWFs")),
+        (u.innerText = Base64.decode("U3VwcG9ydA=="));
 }
-
-else if(isNaN(i)) {
-	a=64
+function a() {
+    window.open(Base64.decode("aHR0cHM6Ly93d3cueW91dHViZS5jb20vY2hhbm5lbC9VQ2g2WnYzc2ZyYVdoTGlyOW5DWFQ3bnc="));
 }
-
-t=t+this._keyStr.charAt(s)+this._keyStr.charAt(o)+this._keyStr.charAt(u)+this._keyStr.charAt(a)
+function b() {
+    window.open(Base64.decode("aHR0cHM6Ly9naXRodWIuY29tL0xvdmVEb0xvdmU="));
 }
-
-return t
-},decode:function(e) {
-var t="";var n,r,i;var s,o,u,a;var f=0;e=e.replace(/[^A-Za-z0-9\+\/\=]/g,"");while(f<e.length){s=this._keyStr.indexOf(e.charAt(f++));o=this._keyStr.indexOf(e.charAt(f++));u=this._keyStr.indexOf(e.charAt(f++));a=this._keyStr.indexOf(e.charAt(f++));n=s<<2|o>>4;r=(o&15)<<4|u>>2;i=(u&3)<<6|a;t=t+String.fromCharCode(n);if(u!=64){t=t+String.fromCharCode(r)
+function c() {
+    window.open(Base64.decode("aHR0cHM6Ly90d2l0dGVyLmNvbS9Mb3ZlRG9Mb3ZlMQ=="));
 }
-
-if(a!=64) {
-t=t+String.fromCharCode(i)
-}}
-
-t=Base64._utf8_decode(t);return t
-},_utf8_encode:function(e) {
-e=e.replace(/\r\n/g,"\n");var t="";for(var n=0;n<e.length;n++){var r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r)
+function d() {
+    window.open(Base64.decode("aHR0cHM6Ly9kaXNjb3JkLmdnLzRiNHVETXFWQ3k="));
 }
-
-else if(r>127&&r<2048) {
-t+=String.fromCharCode(r>>6|192);t+=String.fromCharCode(r&63|128)
+function e() {
+    window.open(Base64.decode("aHR0cHM6Ly90Lm1lL0xvdmVEb0xvdmVDaGF0c0dyb3Vw"));
 }
-
-else {
-t+=String.fromCharCode(r>>12|224);t+=String.fromCharCode(r>>6&63|128);t+=String.fromCharCode(r&63|128)
-}}
-
-return t
-},_utf8_decode:function(e) {
-var t="";var n=0;var r=c1=c2=0;while(n<e.length){r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r);n++
+function f() {
+    window.open(Base64.decode("aHR0cHM6Ly9iaXQubHkvMzFlVEVYaA=="));
 }
-
-else if(r>191&&r<224) {
-c2=e.charCodeAt(n+1);t+=String.fromCharCode((r&31)<<6|c2&63);n+=2
+function g() {
+    window.open((document.src = Base64.decode("L21haW4vbWFpbi5odG1s")));
 }
-
-else {
-c2=e.charCodeAt(n+1);c3=e.charCodeAt(n+2);t+=String.fromCharCode((r&15)<<12|(c2&63)<<6|c3&63);n+=3
-}}
-
-return t
-}}
-
-function startup()
-{
-   var tag = document.getElementById("tag");
-   var yt = document.getElementById("youtube");
-   var git = document.getElementById("github");
-   var twitter = document.getElementById("twitter");
-   var dc = document.getElementById("discord");
-   var telegram = document.getElementById("telegram");
-   var donate = document.getElementById("donate");
-   var main = document.getElementById("main");
-   var store = document.getElementById("store");
-   var adb = document.getElementById("adb");
-   var support = document.getElementById("support");
-
-   yt.addEventListener("click", a);
-   git.addEventListener("click", b);
-   twitter.addEventListener("click", c);
-   dc.addEventListener("click", d);
-   telegram.addEventListener("click", e);
-   donate.addEventListener("click", f);
-   main.addEventListener("click", g);
-   store.addEventListener("click", h);
-   adb.addEventListener("click", i);
-   support.addEventListener("click", j);
-
-   tag.innerText = Base64.decode('QExvdmVEb0xvdmU=');
-   yt.innerText = Base64.decode('WW91dHViZQ==');
-   git.innerText = Base64.decode('R2l0aHVi');
-   twitter.innerText = Base64.decode('VHdpdHRlcg==');
-   dc.innerText = Base64.decode('RGlzY29yZA==');
-   telegram.innerText = Base64.decode('VGVsZWdyYW0=');
-   donate.innerText = Base64.decode('RG9uYXRl');
-   main.innerText = Base64.decode('Q2xpY2sgVG8gU3RhcnQgVXNpbmcgVGhlIFdlYnNpdGU=');
-   store.innerText = Base64.decode('TG92ZURvTG92ZSBTdG9yZQ==');
-   adb.innerText = Base64.decode('QURCIFR1dG9yaWFs');
-   support.innerText = Base64.decode('U3VwcG9ydA==');
+function h() {
+    window.open((document.src = Base64.decode("L3N0b3JlL3N0b3JlLmh0bWw=")));
 }
-
-function a()
-{
-   window.open(Base64.decode('aHR0cHM6Ly93d3cueW91dHViZS5jb20vY2hhbm5lbC9VQ2g2WnYzc2ZyYVdoTGlyOW5DWFQ3bnc='));
+function i() {
+    window.open((document.src = Base64.decode("L2FkYi9hZGIuaHRtbA==")));
 }
-
-function b()
-{
-   window.open(Base64.decode('aHR0cHM6Ly9naXRodWIuY29tL0xvdmVEb0xvdmU='));
-}
-
-function c()
-{
-   window.open(Base64.decode('aHR0cHM6Ly90d2l0dGVyLmNvbS9Mb3ZlRG9Mb3ZlMQ=='));
-}
-
-function d()
-{
-   window.open(Base64.decode('aHR0cHM6Ly9kaXNjb3JkLmdnLzRiNHVETXFWQ3k='));
-}
-
-function e()
-{
-   window.open(Base64.decode('aHR0cHM6Ly90Lm1lL0xvdmVEb0xvdmVDaGF0c0dyb3Vw'));
-}
-
-function f()
-{
-   window.open(Base64.decode('aHR0cHM6Ly9iaXQubHkvMzFlVEVYaA=='));
-}
-
-function g()
-{
-   window.open(document.src = Base64.decode('L21haW4vbWFpbi5odG1s'));
-}
-
-function h()
-{
-   window.open(document.src = Base64.decode('L3N0b3JlL3N0b3JlLmh0bWw='));
-}
-
-function i()
-{
-   window.open(document.src = Base64.decode('L2FkYi9hZGIuaHRtbA=='));
-}
-
-function j()
-{
-   window.open(document.src = Base64.decode("L3N1cHBvcnQvc3VwcG9ydC5odG1s"));
+function j() {
+    window.open((document.src = Base64.decode("L3N1cHBvcnQvc3VwcG9ydC5odG1s")));
 }
--- support/support.js
+++ support/support.js
@@ -1,1 +1,98 @@
-window.onload=start;var Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(e){var t,r,d,a,o,n,l,h="",c=0;for(e=Base64._utf8_encode(e);c<e.length;)t=e.charCodeAt(c++),r=e.charCodeAt(c++),d=e.charCodeAt(c++),a=t>>2,o=(3&t)<<4|r>>4,n=(15&r)<<2|d>>6,l=63&d,isNaN(r)?n=l=64:isNaN(d)&&(l=64),h=h+this._keyStr.charAt(a)+this._keyStr.charAt(o)+this._keyStr.charAt(n)+this._keyStr.charAt(l);return h},decode:function(e){var t,r,d,a,o,n,l,h="",c=0;for(e=e.replace(/[^A-Za-z0-9\+\/\=]/g,"");c<e.length;)a=this._keyStr.indexOf(e.charAt(c++)),o=this._keyStr.indexOf(e.charAt(c++)),n=this._keyStr.indexOf(e.charAt(c++)),l=this._keyStr.indexOf(e.charAt(c++)),t=a<<2|o>>4,r=(15&o)<<4|n>>2,d=(3&n)<<6|l,h+=String.fromCharCode(t),64!=n&&(h+=String.fromCharCode(r)),64!=l&&(h+=String.fromCharCode(d));return Base64._utf8_decode(h)},_utf8_encode:function(e){e=e.replace(/\r\n/g,"\n");for(var t="",r=0;r<e.length;r++){var d=e.charCodeAt(r);d<128?t+=String.fromCharCode(d):d>127&&d<2048?(t+=String.fromCharCode(d>>6|192),t+=String.fromCharCode(63&d|128)):(t+=String.fromCharCode(d>>12|224),t+=String.fromCharCode(d>>6&63|128),t+=String.fromCharCode(63&d|128))}return t},_utf8_decode:function(e){for(var t="",r=0,d=c1=c2=0;r<e.length;)(d=e.charCodeAt(r))<128?(t+=String.fromCharCode(d),r++):d>191&&d<224?(t+=String.fromCharCode((31&d)<<6|63&(c2=e.charCodeAt(r+1))),r+=2):(t+=String.fromCharCode((15&d)<<12|(63&(c2=e.charCodeAt(r+1)))<<6|63&(c3=e.charCodeAt(r+2))),r+=3);return t}};function start(){document.getElementById("submit").addEventListener("click",check);var e=document.getElementById("headTitle"),t=document.getElementById("formLegend"),r=document.getElementById("userName"),d=document.getElementById("Email"),a=document.getElementById("Phone"),o=document.getElementById("titleQuestion"),n=document.getElementById("Question");e.innerText=Base64.decode("V2VsY29tZSBUbyBTdXBwb3J0IFBhZ2U="),t.innerText=Base64.decode("RW1haWwgVXM="),r.innerText=Base64.decode("VXNlcm5hbWUgOiA="),d.innerText=Base64.decode("RW1haWwgOiA="),a.innerText=Base64.decode("UGhvbmUgTnVtYmVyIDog"),o.innerText=Base64.decode("UXVlc3Rpb24gVGl0bGUgOiA="),n.innerText=Base64.decode("UXVlc3Rpb24gQ29udGVudCBIZXJlIDog")}function check(){var e=document.getElementById("username"),t=document.getElementById("email"),r=document.getElementById("phone"),d=document.getElementById("titlequestion");if(""==e.value)alert(Base64.decode("UGxlYXNlIGVudGVyIHVzZXJuYW1lISEh"));else if(""==t.value)alert(Base64.decode("UGxlYXNlIGVudGVyIGVtYWlsISEh"));else if(r.value.length<7)alert(Base64.decode("UGxlYXNlIGVudGVyIHBob25lIG51bWJlciBjb3JyZWN0bHkhISE="));else if(""==d.value)alert(Base64.decode("UGxlYXNlIGVudGVyIHF1ZXN0aW9uIHRpdGxlISEh"));else if(""!=t.value&&""!=r.value&&r.value.length>7&&""!=d)return window.alert(Base64.decode("WW91ciBxdWVzdGlvbiB3aWxsIHNlbmQgdG8gYWRtaW4gbm93ISEh")),document.getElementById("forms")}
+window.onload = start;
+var Base64 = {
+    _keyStr: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
+    encode: function (e) {
+        var t,
+            r,
+            d,
+            a,
+            o,
+            n,
+            l,
+            h = "",
+            c = 0;
+        for (e = Base64._utf8_encode(e); c < e.length; )
+            (t = e.charCodeAt(c++)),
+                (r = e.charCodeAt(c++)),
+                (d = e.charCodeAt(c++)),
+                (a = t >> 2),
+                (o = ((3 & t) << 4) | (r >> 4)),
+                (n = ((15 & r) << 2) | (d >> 6)),
+                (l = 63 & d),
+                isNaN(r) ? (n = l = 64) : isNaN(d) && (l = 64),
+                (h = h + this._keyStr.charAt(a) + this._keyStr.charAt(o) + this._keyStr.charAt(n) + this._keyStr.charAt(l));
+        return h;
+    },
+    decode: function (e) {
+        var t,
+            r,
+            d,
+            a,
+            o,
+            n,
+            l,
+            h = "",
+            c = 0;
+        for (e = e.replace(/[^A-Za-z0-9\+\/\=]/g, ""); c < e.length; )
+            (a = this._keyStr.indexOf(e.charAt(c++))),
+                (o = this._keyStr.indexOf(e.charAt(c++))),
+                (n = this._keyStr.indexOf(e.charAt(c++))),
+                (l = this._keyStr.indexOf(e.charAt(c++))),
+                (t = (a << 2) | (o >> 4)),
+                (r = ((15 & o) << 4) | (n >> 2)),
+                (d = ((3 & n) << 6) | l),
+                (h += String.fromCharCode(t)),
+                64 != n && (h += String.fromCharCode(r)),
+                64 != l && (h += String.fromCharCode(d));
+        return Base64._utf8_decode(h);
+    },
+    _utf8_encode: function (e) {
+        e = e.replace(/\r\n/g, "\n");
+        for (var t = "", r = 0; r < e.length; r++) {
+            var d = e.charCodeAt(r);
+            d < 128
+                ? (t += String.fromCharCode(d))
+                : d > 127 && d < 2048
+                ? ((t += String.fromCharCode((d >> 6) | 192)), (t += String.fromCharCode((63 & d) | 128)))
+                : ((t += String.fromCharCode((d >> 12) | 224)), (t += String.fromCharCode(((d >> 6) & 63) | 128)), (t += String.fromCharCode((63 & d) | 128)));
+        }
+        return t;
+    },
+    _utf8_decode: function (e) {
+        for (var t = "", r = 0, d = (c1 = c2 = 0); r < e.length; )
+            (d = e.charCodeAt(r)) < 128
+                ? ((t += String.fromCharCode(d)), r++)
+                : d > 191 && d < 224
+                ? ((t += String.fromCharCode(((31 & d) << 6) | (63 & (c2 = e.charCodeAt(r + 1))))), (r += 2))
+                : ((t += String.fromCharCode(((15 & d) << 12) | ((63 & (c2 = e.charCodeAt(r + 1))) << 6) | (63 & (c3 = e.charCodeAt(r + 2))))), (r += 3));
+        return t;
+    },
+};
+function start() {
+    document.getElementById("submit").addEventListener("click", check);
+    var e = document.getElementById("headTitle"),
+        t = document.getElementById("formLegend"),
+        r = document.getElementById("userName"),
+        d = document.getElementById("Email"),
+        a = document.getElementById("Phone"),
+        o = document.getElementById("titleQuestion"),
+        n = document.getElementById("Question");
+    (e.innerText = Base64.decode("V2VsY29tZSBUbyBTdXBwb3J0IFBhZ2U=")),
+        (t.innerText = Base64.decode("RW1haWwgVXM=")),
+        (r.innerText = Base64.decode("VXNlcm5hbWUgOiA=")),
+        (d.innerText = Base64.decode("RW1haWwgOiA=")),
+        (a.innerText = Base64.decode("UGhvbmUgTnVtYmVyIDog")),
+        (o.innerText = Base64.decode("UXVlc3Rpb24gVGl0bGUgOiA=")),
+        (n.innerText = Base64.decode("UXVlc3Rpb24gQ29udGVudCBIZXJlIDog"));
+}
+function check() {
+    var e = document.getElementById("username"),
+        t = document.getElementById("email"),
+        r = document.getElementById("phone"),
+        d = document.getElementById("titlequestion");
+    if ("" == e.value) alert(Base64.decode("UGxlYXNlIGVudGVyIHVzZXJuYW1lISEh"));
+    else if ("" == t.value) alert(Base64.decode("UGxlYXNlIGVudGVyIGVtYWlsISEh"));
+    else if (r.value.length < 7) alert(Base64.decode("UGxlYXNlIGVudGVyIHBob25lIG51bWJlciBjb3JyZWN0bHkhISE="));
+    else if ("" == d.value) alert(Base64.decode("UGxlYXNlIGVudGVyIHF1ZXN0aW9uIHRpdGxlISEh"));
+    else if ("" != t.value && "" != r.value && r.value.length > 7 && "" != d) return window.alert(Base64.decode("WW91ciBxdWVzdGlvbiB3aWxsIHNlbmQgdG8gYWRtaW4gbm93ISEh")), document.getElementById("forms");
+}

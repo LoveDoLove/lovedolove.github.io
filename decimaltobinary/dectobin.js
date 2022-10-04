258cb74f65c216974b2a78cfbed3c566dc523075
LoveDoLove <v0130p5100cuboss@gmail.com>
2022-10-04 21:23:12 +0800

    update

--- decimaltobinary/dectobin.js
+++ decimaltobinary/dectobin.js
@@ -1,13 +1,1 @@
-function convertToBinary(x) {
-  x = document.getElementById("decimal").value;
-  let bin = 0;
-  let rem,
-    i = 1;
-  while (x != 0) {
-    rem = x % 2;
-    x = parseInt(x / 2);
-    bin = bin + rem * i;
-    i = i * 10;
-  }
-  document.getElementById("binary").value = bin;
-}
+function convertToBinary(e){e=document.getElementById("decimal").value;let n,t=0,a=1;for(;0!=e;)n=e%2,e=parseInt(e/2),t+=n*a,a*=10;document.getElementById("binary").value=t}
